7309d28c784c787a70a218d47c39e88603f24665
Happy1speed <144567994+Happy1speed@users.noreply.github.com>
2025-02-10 20:58:19 -0800

    Witch tweaks and textures

--- src/main/java/net/happyspeed/raid_on/mixin/WitchEntityMixin.java
+++ src/main/java/net/happyspeed/raid_on/mixin/WitchEntityMixin.java
@@ -1,25 +1,31 @@
 package net.happyspeed.raid_on.mixin;
 
 
-import net.happyspeed.raid_on.RaidonMod;
 import net.happyspeed.raid_on.config.ModConfigs;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.ai.RangedAttackMob;
-import net.minecraft.entity.ai.goal.Goal;
-import net.minecraft.entity.ai.goal.GoalSelector;
-import net.minecraft.entity.boss.ServerBossBar;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.mob.WitchEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.thrown.PotionEntity;
 import net.minecraft.entity.raid.RaiderEntity;
-import net.minecraft.sound.SoundEvent;
-import net.minecraft.text.Text;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionUtil;
+import net.minecraft.potion.Potions;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
-import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(WitchEntity.class)
 public abstract class WitchEntityMixin extends RaiderEntity {
+
+    @Shadow public abstract boolean isDrinking();
 
     protected WitchEntityMixin(EntityType<? extends RaiderEntity> entityType, World world) {
         super(entityType, world);
@@ -34,5 +40,16 @@ public abstract class WitchEntityMixin extends RaiderEntity {
         else {
             return 60;
         }
+    }
+
+    @Redirect(method = "attack", at= @At(value = "INVOKE", target = "Lnet/minecraft/potion/PotionUtil;setPotion(Lnet/minecraft/item/ItemStack;Lnet/minecraft/potion/Potion;)Lnet/minecraft/item/ItemStack;"))
+    private ItemStack redirectLingering(ItemStack stack, Potion potion) {
+        if (this.hasActiveRaid() && !(this.getTarget() instanceof RaiderEntity) && !this.isDrinking()) {
+            if (this.random.nextBetween(1, 5) == 1) {
+                return PotionUtil.setPotion(new ItemStack(Items.LINGERING_POTION), potion);
+            }
+        }
+        stack = PotionUtil.setPotion(new ItemStack(Items.SPLASH_POTION), potion);
+        return stack;
     }
 }
